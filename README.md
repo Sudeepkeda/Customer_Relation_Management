5a388e31a157a1374bef67da0569317d0b8f0c18
SUDEEP <peedus.1177@gmail.com>
2025-09-03 12:50:05 +0530

    added .gitignore to ignore unncessary files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Customer_Relation_Management
-This is the first Project.
--- crm_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PasswordResetRequestView, PasswordResetConfirmView
-
-urlpatterns = [
-    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-]
